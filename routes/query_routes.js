2693fb6b66fda809a045bff0caf55ad06ab72c91
bhargavtechisor <bhargavtechisor@gmail.com>
2025-02-20 11:25:41 +0530

    add route

--- routes/query_routes.js
+++ routes/query_routes.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const query = require("../controller/ticketing");
 // const Query = require("./controller/Query")
+const signUp = require("../controller/admin/signup");
+const loginpage = require("../controller/admin/login");
 
-router.post("/generateToken", query.generateToken );
-router.post("/verify",query.verify);
-router.get("/getQuery",query.getQuery)
-router.get("/getDescriptions",query.getDescriptions)
+router.post("/generateToken", query.generateToken);
+router.post("/verify", query.verify);
+router.get("/getQuery", query.getQuery);
+router.get("/getDescriptions", query.getDescriptions);
 
+//Admin login and sign Up
+router.post("/signup", signUp);
+router.post("/login", loginpage);
 
 module.exports = router;
