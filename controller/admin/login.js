601d5e8632acb00b5dae08981b980f68c1721512
bhargavtechisor <bhargavtechisor@gmail.com>
2025-02-21 14:17:37 +0530

    sign up change

--- controller/admin/login.js
+++ controller/admin/login.js
@@ -1,33 +1,33 @@
 const User = require("../../modal/admin/signup");
 const cookieParser = require("cookie-parser");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 exports.loginpage = async (req, res) => {
   try {
     const { email, password } = req.body;
     console.log(req.body);
-    if(!email || !password){
+    if (!email || !password) {
       return res.status(400).json({
-          success:false,
-          message:"Invalid Email or password"
-      })
-  }
+        success: false,
+        message: "Invalid Email or password",
+      });
+    }
 
     const user = await User.findOne({ email });
     const isValidPassword = await bcrypt.compare(password, user.password);
 
     if (isValidPassword) {
-     await res.cookie("user", JSON.stringify(user), {
-          httpOnly: true, // Prevents JavaScript from accessing it (for security)
-          secure: false, // Change to `true` in production (requires HTTPS)
-          sameSite: "Lax", // Helps prevent CSRF attacks
-        });
+      await res.cookie("user", JSON.stringify(user), {
+        httpOnly: true, // Prevents JavaScript from accessing it (for security)
+        secure: false, // Change to `true` in production (requires HTTPS)
+        sameSite: "Lax", // Helps prevent CSRF attacks
+      });
       return res.status(200).json({
-          success: true,
-          message: "User login successfully",
-          user: user
-          });
-  }
+        success: true,
+        message: "User login successfully",
+        user: user,
+      });
+    }
 
     // if (!user)
     //   return res.status(400).json({ message: " Invalid email or password" });
@@ -45,14 +45,13 @@ exports.me = async (req, res) => {
   }
 
   return res.json({ success: true, user: JSON.parse(userCookie) });
-}
+};
 
 exports.logout = async (req, res) => {
-      res.clearCookie("user");
-      res.json({
-          success:true,
-           message: "Logged out successfully"
-         });
- 
-}
+  res.clearCookie("user");
+  res.json({
+    success: true,
+    message: "Logged out successfully",
+  });
+};
 // module.exports = loginpage;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express= require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 const cookieParser = require("cookie-parser");
@@ -11,12 +11,11 @@ app.use(cors({origin:["http://localhost:5173"],
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    });
+  console.log(`Server is running on port ${PORT}`);
+});
 
-
-const connectDB = require("./Config/dbConnection")
+const connectDB = require("./Config/dbConnection");
 connectDB();
 
 const query_routes = require("./routes/query_routes");
-app.use("/api",query_routes);
+app.use("/api", query_routes);
